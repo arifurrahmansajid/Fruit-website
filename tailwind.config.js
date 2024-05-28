fc3915838fd1ee1bd22c92f2c863baf57416d986
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2024-05-28 07:57:57 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        xsm: '500px'
+      },
+      spacing:{
+        13: '3.25rem'
+      },
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '1.2' }],
+      }
+    },
+  },
+  plugins: [],
+}
+
